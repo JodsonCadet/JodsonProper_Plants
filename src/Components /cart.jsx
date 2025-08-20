111e94f0edd6a52a594b303e01ee67dcd4da7293
JodsonCadet <cadetjoddson@gmail.com>
2025-08-19 23:25:26 -0400

    revise commit

--- src/Components /cart.jsx
+++ src/Components /cart.jsx
@@ -1,47 +1,26 @@
-import PLANTS from "../data";
-import { useContext } from "react";
-import { useState } from "react";
-import { FlowerContext } from "./FlowerContext";
-// import { FlowerContext } from "./FlowerContext.js";
+import PLANTS from '../data'
+import { useState } from 'react'
+// import { FlowerContext } from './Components/FlowerContext'
+import { useContext } from 'react'
 
-export default function Cart({ flowerButtonClick }) {
-  const [plusButtonClick, minusButtonClick] = useState([]);
-  const { cart, flowerButtonClick } = useContext(FlowerContext);
-  console.log(flowerButtonClick)
-  console.log(cart)
+export default function Cart({ flowerButtonClick}) {
+    const [plusButtonCLick, minusButtonCLick] = useState([])
+    const flowerArr = useContext(flowerButtonClick)
+    console.log(flowerArr)
 
-  const plusButtonClick = (flower) => {
-    setCart(cart => [...cart, flower]);
-  }
-
-  const minusButtonClick = (flower) => {
-    setCart(cart => 
-    {
-      const index = cart.findIndex(item => item.id === flower.id); // remove all instances
-      if (index !== -1) {
-        return cart.filter((_, i) => i !== index);
-      }
-      return cart
-    })
-  }
-
-  // add function called cart filter and map all items into IDs 
+    
 
   return (
     <>
-      <div>
-        <h2>Shopping Cart </h2>
-        {Object.values(PLANTS).map((item, flower) => (
-          <div key={flower}>
-            <p>{item.name}</p>
-            <p> {item.image}</p>
-            <p>{item.id}</p>
-            <button onClick={() => plusButtonClick(cart)}>+</button>
-            <button onClick={() => minusButtonClick(cart)}>-</button>
-          
-          </div>
-        ))}
-      </div>
+        <div>
+            {Object.values(PLANTS).map((item, flower) => (
+                <div key={flower}>
+                    <p>{item.image}</p>
+                    <button onClick={() => plusButtonCLick(flower)}>+</button>
+                    <button onClick={() => minusButtonCLick(flower)}>-</button>
+                </div>
+            ))}  
+        </div>
     </>
-  );
+  )
 }
--- src/Components /flowerButtons.jsx
+++ src/Components /flowerButtons.jsx
@@ -1,39 +1,32 @@
-import "./FlowerButtons.css";
-import PLANTS from "../data";
-import { useState } from "react";
-import { FlowerContext } from "./FlowerContext.js";
+import './flowerButtons.css'
+import PLANTS from '../data'
+import { useState } from 'react'
+import { FlowerContext } from './FlowerContext'
 
 
 export default function FlowerButtons({ children }) {
-  const [cart, setCart] = useState([])
+    const [cart, setCart] = useState([])
+    const flowerMap = PLANTS.reduce((combine, flower) => {
+        combine[flower.id] = flower
+        return combine
+    },{})
+    const flowerButtonClick = (flower) => {
+        setCart(cart => [...cart, flower])
+        console.log(cart)
+    }
 
-  const flowerMap = PLANTS.reduce((combine, PLANTS) => {
-    combine[PLANTS.id] = PLANTS;
-    return combine;
-  }, {});
-  const flowerButtonClick = (flower) => {
-    setCart((cart) => [...cart, flower]);
-  }
-  console.log(cart);
-  const value = {
-    cart,
-    flowerButtonClick,
-    setCart,
-  }
   return (
-    
-    <div className="plants">
-      {Object.values(flowerMap).map((flower) => (
-        <div className="items" key={flower.id}>
-          <h3>{flower.name}</h3>
-          <p className="flower">{flower.image}</p>
-          <br />
-          <button onClick={() => flowerButtonClick(flower)}>Add To Cart</button>
-        </div>
-      ))}
-      <FlowerContext.Provider value={flowerButtonClick}>
-        {children}
-      </FlowerContext.Provider>
+    <div  className="plants">
+        {Object.values(flowerMap).map(flower => (
+            <div className="items" key={flower.id}>
+                <h3>{flower.name}</h3>
+                <p className='flower'>{flower.image}</p>
+                <br />
+                <button onClick={() => flowerButtonClick(flower)}>Add To Cart</button>
+            </div>
+        ))}
+        {/* for some reason when I add {children my items on my webpage is not displaying} */}
+        <FlowerContext.Provider value={{flowerButtonClick}}></FlowerContext.Provider>  
     </div>
   );
 }
